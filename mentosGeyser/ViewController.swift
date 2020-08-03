057cbfdbf734b99e62130465d999738fc25e6bf8
TakeruYufune <27349791+TakeruYufune@users.noreply.github.com>
2020-08-04 06:40:03 +0900

    shake & segue done

--- mentosGeyser/ViewController.swift
+++ mentosGeyser/ViewController.swift
@@ -13,26 +13,22 @@ class ViewController: UIViewController{
     
     let motionManager = CMMotionManager()
 
+
+    @IBOutlet weak var box: UIView!
+    
     @IBOutlet weak var accX: UILabel!
     @IBOutlet weak var accY: UILabel!
     @IBOutlet weak var accZ: UILabel!
     @IBOutlet weak var splash: UILabel!
     
+    
     var x = 0.0
     var y = 0.0
     var z = 0.0
-    var count = 0
+    var count = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        /*
-        let screenWidth = self.view.bounds.width
-        let screenHeight = self.view.bounds.height
-        
-        let testDraw = ObjectDraw(frame: CGRect(x: 0, y: 0,
-                       width: screenWidth, height: screenHeight))
-        self.view.addSubview(testDraw)
-*/
         
         if motionManager.isAccelerometerAvailable {
             // intervalの設定 [sec]
@@ -47,19 +43,25 @@ class ViewController: UIViewController{
         }
     }
     
+    private func animateFrame() {
+        UIView.animate(withDuration: 1.0, delay: 0, options: .curveLinear, animations: {
+            self.box.frame.origin.y -= 10
+        }, completion: nil)
+    }
+    
     func outputAccelData(acceleration: CMAcceleration){
         // 加速度センサー [G]
         accX.text = String(format: "%06f", acceleration.x)
         accY.text = String(format: "%06f", acceleration.y)
         accZ.text = String(format: "%06f", acceleration.z)
         
-        print(abs(z - acceleration.z))
-        
         if abs(x - acceleration.x) > 0.3 || abs(y - acceleration.y) > 0.3 ||
         abs(z - acceleration.z) > 0.3{
             view.backgroundColor = UIColor.red
             //shake判定点数を付与
             count += 1
+            
+            animateFrame()
         } else {
             view.backgroundColor = UIColor.white
         }
@@ -68,8 +70,9 @@ class ViewController: UIViewController{
         y = acceleration.y
         z = acceleration.z
         
-        if count > 100{
+        if count == 100{
             splash.text = "SPLASH!!"
+            self.performSegue(withIdentifier: "splash", sender: nil)
         }
         
    }
