d730e977ee14af45a305991dbe584d42499888ef
TakeruYufune <27349791+TakeruYufune@users.noreply.github.com>
2020-08-04 00:02:34 +0900

    Initial Commit

---
+++ mentosGeyser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mentosGeyser
+//
+//  Created by Takeru Yufune on 2020/08/04.
+//  Copyright © 2020 Takeru Yufune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
